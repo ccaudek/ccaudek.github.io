87f105df5113f2ce6f0dbca2e1c428896f20672c
Corrado Caudek <corrado.caudek@unifi.it>
2025-01-26 09:21:31 +0100

    added Google Analytics tag

--- images/favicon/favicon.R
+++ images/favicon/favicon.R
@@ -3,7 +3,9 @@ library(magick)
 
 # Function to calculate hexagon vertices
 
-deg2rad <- function(deg) { return(deg * (pi / 180)) }
+deg2rad <- function(deg) {
+  return(deg * (pi / 180))
+}
 
 hexagon_vertices <- function(center_x, center_y, radius) {
   angles_deg <- seq(90, -270, by = -60) # Starting from the top vertex and moving clockwise
@@ -13,42 +15,43 @@ hexagon_vertices <- function(center_x, center_y, radius) {
   return(data.frame(x = x, y = y))
 }
 
-# Make plot 
+# Make plot
 
-favicon <- ggplot() + 
-    # Make outer hexagon
-    geom_polygon(
-        data = round(hexagon_vertices(0, 0, 3), 6), 
-        aes(x = x, y = y), 
-        fill = '#2c3f51'
-    ) +
-    # Make inner hexagon
-    geom_polygon(
-      data = round(hexagon_vertices(0, 0, 2.7), 6), 
-      aes(x = x, y = y), 
-      fill = '#5480a7'
-    ) +
-    # Add J
-    geom_text(
-      aes(x = 0, y = 0, label = 'j'), 
-      hjust = 0.5, 
-      nudge_y = 0.4,
-      size = 90,
-      family = 'Raleway', 
-      fontface = 'bold', 
-      color = 'white'
-    ) +
-    coord_fixed() +
-    theme_void() +
-    theme(
-        plot.background = element_rect(fill = "transparent", colour = NA),
-        panel.background = element_rect(fill = "transparent", colour = NA)
-    )
+favicon <- ggplot() +
+  # Make outer hexagon
+  geom_polygon(
+    data = round(hexagon_vertices(0, 0, 3), 6),
+    aes(x = x, y = y),
+    fill = "#2c3f51"
+  ) +
+  # Make inner hexagon
+  geom_polygon(
+    data = round(hexagon_vertices(0, 0, 2.7), 6),
+    aes(x = x, y = y),
+    fill = "#5480a7"
+  ) +
+  # Add J
+  geom_text(
+    aes(x = 0, y = 0, label = "c"),
+    hjust = 0.5,
+    nudge_y = 0.4,
+    size = 90,
+    family = "Raleway",
+    fontface = "bold",
+    color = "white"
+  ) +
+  coord_fixed() +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "transparent", colour = NA),
+    panel.background = element_rect(fill = "transparent", colour = NA)
+  )
 
 ggsave(
-  'favicon.pdf', favicon, height = 6, width = 6, 
+  "favicon.pdf", favicon,
+  height = 6, width = 6,
   bg = "transparent", device = cairo_pdf
 )
-renderthis::to_png('favicon.pdf', 'favicon.png', density = 300)
-favicon <- image_scale(image_read('favicon.png'), '32')
+renderthis::to_png("favicon.pdf", "favicon.png", density = 300)
+favicon <- image_scale(image_read("favicon.png"), "32")
 image_write(favicon, path = "favicon.ico", format = "png")
